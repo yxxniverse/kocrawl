eb8ac195c9375490625f7cb1200827082dd494b9
yxxniverse <71762611+yxxniverse@users.noreply.github.com>
2022-04-07 14:33:35 +0900

    Update term.py

--- kocrawl/term.py
+++ kocrawl/term.py
@@ -7,27 +7,27 @@ from kocrawl.searcher.term_searcher import TermSearcher
 
 class TermCrawler(BaseCrawler):
 
-    def request(self, term: str) -> str:
+    def request(self, eco: str) -> str:
 
         try:
-            return self.request_debug(term)[0]
+            return self.request_debug(eco)[0]
         except Exception:
             return TermAnswerer().sorry(
                 "해당 단어에 대한 정보가 없어요."
             )
 
-    def request_dict(self, term: str):
+    def request_dict(self, eco: str):
 
         try:
-            return self.request_debug(term)[1]
+            return self.request_debug(eco)[1]
         except Exception:
             return TermAnswerer().sorry(
                 "해당 단어에 대한 정보가 없어요."
             )
 
-    def request_debug(self, term: str):
+    def request_debug(self, eco: str):
 
-        result_dict = TermSearcher().search_term(term)
+        result_dict = TermSearcher().search_term(eco)
         result_dict = TermEditor().edit_term(result_dict)
         result = TermAnswerer().term_form(result_dict)
         return result, result_dict
